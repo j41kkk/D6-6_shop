8f985c9727e45851b7002b9d17b0a35451e3ad05
Юрий <124659396+j41kkk@users.noreply.github.com>
2023-03-14 09:53:09 +0300

    Add files via upload

---
+++ simpleapp/views.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from django.views.generic import ListView, DetailView
+from .models import Product
+
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products.html'
+    context_object_name = 'products'
+
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = None
+        return context
+
